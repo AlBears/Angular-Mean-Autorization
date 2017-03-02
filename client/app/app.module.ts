bbf593d975d744b397c83212c5012a3bac6a18a5
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-02 15:30:57 +0300

    lazy loading. auth module

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+
 import { HttpModule } from '@angular/http';
 
 import { MessageModule } from './messages/message.module';
@@ -9,9 +9,6 @@ import { AppComponent }  from './app.component';
 
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
-import { LogoutComponent } from './auth/logout.component';
-import { SigninComponent } from './auth/signin.component';
-import { SignupComponent } from './auth/signup.component';
 import { ErrorComponent } from './errors/error.component';
 
 import { AuthService } from './auth/auth.service';
@@ -24,7 +21,6 @@ import { AppRoutingModule } from './app.routing';
   imports:      [ 
                   BrowserModule, 
                   AppRoutingModule,
-                  ReactiveFormsModule,
                   HttpModule,
                   MessageModule
                 ],
@@ -32,9 +28,6 @@ import { AppRoutingModule } from './app.routing';
                   AppComponent,
                   AuthenticationComponent,
                   HeaderComponent,
-                  LogoutComponent,
-                  SigninComponent,
-                  SignupComponent,
                   ErrorComponent
                 ],
   providers:    [ AuthService, ErrorService ],
--- client/app/app.routing.ts
+++ client/app/app.routing.ts
@@ -3,15 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MessagesComponent } from './messages/messages.component';
 import { AuthenticationComponent } from './auth/authentication.component';
-import { SigninComponent } from './auth/signin.component';
-import { SignupComponent } from './auth/signup.component';
-
-import { AUTH_ROUTES } from './auth/auth.routes';
 
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/messages', pathMatch: 'full' },
     { path: 'messages', component: MessagesComponent },
-    { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
+    { path: 'auth', component: AuthenticationComponent, loadChildren: 'app/auth/auth.module#AuthModule' }
 ];
 
 @NgModule({
